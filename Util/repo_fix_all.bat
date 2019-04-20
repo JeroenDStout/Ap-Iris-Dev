c38a7279274a682e6586066ca5ee278c615ff539
Jeroen D Stout <jeroenstout@gmail.com>
2019-04-20 16:36:41 +0200

    Replace fix_all with update_all, using pnpm and the submodule fix

--- Util/repo_fix_all.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-echo --
-echo -- Fix Submodules
-echo --
-
-python "Repo/util_fixsubmodules.py"
-
-pause
---
+++ Util/repo_update_all.bat
@@ -0,0 +1,17 @@
+@echo off
+
+echo.
+echo --
+echo -- Util Pnpm
+echo --
+echo.
+python "Repo/util_pnpm.py"
+
+echo.
+echo --
+echo -- Util Fix Submodules
+echo --
+echo.
+python "Repo/util_fixsubmodules.py"
+
+pause
