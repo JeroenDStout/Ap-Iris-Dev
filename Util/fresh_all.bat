a0f4cba63ff406c19e42bf0a9e738aa29a66968f
Jeroen D Stout <jeroenstout@gmail.com>
2019-04-20 16:37:08 +0200

    Improve fresh_all

--- Util/fresh_all.bat
+++ Util/fresh_all.bat
@@ -1,18 +1,42 @@
 @echo off
 
-echo --
-echo -- Fresh Pip
-echo --
-python "Repo/fresh_pip.py"
+python "Repo/warning.py"
 
+echo.
 echo --
 echo -- Fresh Templates
 echo --
+echo.
 python "Repo/fresh_templates.py"
 
+echo.
 echo --
-echo -- Fix Submodules
+echo -- Fresh Pip
 echo --
+echo.
+python "Repo/fresh_pip.py"
+
+echo.
+echo --
+echo -- Fresh Npm
+echo --
+echo.
+python "Repo/fresh_npm.py"
+
+echo.
+echo --
+echo -- Util Pnpm
+echo --
+echo.
+python "Repo/util_pnpm.py"
+
+echo.
+echo --
+echo -- Util Fix Submodules
+echo --
+echo.
 python "Repo/util_fixsubmodules.py"
+
+python "Repo/footer.py"
 
 pause
